cb05af04988cb9c764cc15fdcca7a6deaa11dfdb
Josejulianhg466 <josejamerica283@gmail.com>
2024-03-08 10:19:53 -0600

    Nuevo

--- src/main/java/com/appwithspring/demo/controllers/UserController.java
+++ src/main/java/com/appwithspring/demo/controllers/UserController.java
@@ -1,13 +1,16 @@
 package com.appwithspring.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     @GetMapping("details")
-    public String detail(){
-        return"details" ;
+    public String details(Model model){
+        model.addAttribute("title","Hola mundo");
+        model.addAttribute("name","Jose Julian el papucho ;y xd");
+        model.addAttribute("lastname", "Hernandez");
+        return "details"; // Assuming there's a template named "details.html" to render
     }
-
 }
